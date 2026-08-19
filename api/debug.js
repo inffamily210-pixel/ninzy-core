675622bf4e8278d43ab6c841ca1980f2fa6fb85f
inffamily210-pixel <inffamily210@gmail.com>
2026-08-19 18:40:54 +0700

    Refactor debug handler to use parseCards

--- api/debug.js
+++ api/debug.js
@@ -1,17 +1,10 @@
-import * as cheerio from "cheerio";
-import { getHtml } from "./_lib/komiku.js";
+import { LIST_API, getHtml, parseCards } from "./_lib/komiku.js";
 
 export default async function handler(req, res) {
   try {
-    const html = await getHtml("https://api.komiku.org/manga/?orderby=meta_value_num");
-    const $ = cheerio.load(html);
-    const anchors = $('a[href*="/manga/"]');
-
-    res.status(200).json({
-      length: html.length,
-      anchorCount: anchors.length,
-      snippet: html.slice(0, 1500)
-    });
+    const html = await getHtml(`${LIST_API}?orderby=meta_value_num`);
+    const items = parseCards(html);
+    res.status(200).json({ count: items.length, items });
   } catch (err) {
     res.status(500).json({ error: String(err) });
   }
